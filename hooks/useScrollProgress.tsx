e3f9c931b20bea8922e0a5016d907f962e3603f9
sanglap13 <sanglapmridha@gmail.com>
2024-05-16 21:50:31 +0530

    chore: petty change

--- hooks/useScrollProgress.tsx
+++ hooks/useScrollProgress.tsx
@@ -1,4 +1,4 @@
-import React, { use, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const useScrollProgress = () => {
   const [completion, setCompletion] = useState(0);
