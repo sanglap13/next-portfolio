71ad3c8d3795edc27d599ac41a9bf80fad4ca285
sanglap13 <sanglapmridha@gmail.com>
2024-05-19 23:03:15 +0530

    feat: add aboutSection W.I.P

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 //components
+import About from "@/components/sections/About";
 import Hero from "@/components/sections/Hero";
 import Image from "next/image";
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <main>
       <Hero />
+      <About />
     </main>
   );
 }
---
+++ constants/AboutInfo.tsx
@@ -0,0 +1,36 @@
+import {
+  Calendar,
+  GraduationCap,
+  HomeIcon,
+  MailIcon,
+  PhoneCall,
+  User2,
+} from "lucide-react";
+import { text } from "stream/consumers";
+
+export const ABOUT_INFO = [
+  {
+    icon: <User2 size={20} />,
+    text: "Sanglap Mridha",
+  },
+  {
+    icon: <PhoneCall size={20} />,
+    text: "Sanglap Mridha",
+  },
+  {
+    icon: <MailIcon size={20} />,
+    text: "sanglapmridha@gmail.com",
+  },
+  {
+    icon: <Calendar size={20} />,
+    text: "Age 21+",
+  },
+  {
+    icon: <GraduationCap size={20} />,
+    text: "Bachelor of Technology in Computer Science and Engineering",
+  },
+  {
+    icon: <HomeIcon size={20} />,
+    text: "Kolkata, India",
+  },
+];
