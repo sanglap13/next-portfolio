3ab908e3507a3630b5e8505fdc8f00dc974aa646
sanglap13 <sanglapmridha@gmail.com>
2024-05-20 22:42:41 +0530

    feat: add tab section in about me

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -1,7 +1,65 @@
+import Image from "next/image";
 import React from "react";
+import DevImg from "../shared/DevImg";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 
 const About = () => {
-  return <div>About</div>;
+  // const getData = (arr, item) => {
+  //   return arr.find((item) => item.title === title)
+  // }
+  return (
+    <section className="xl:h-[860px] pb-12 xl:py-24">
+      <div className="conatiner mx-auto">
+        <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
+          About Me
+        </h2>
+        <div className="flex flex-col xl:flex-row">
+          {/* image */}
+          <div className="hidden xl:flex flex-1 relative">
+            <DevImg
+              containerStyles="bg-about_shape_light dark:bg-about_shape_dark w-[505px] h-[505px] bg-no-repeat relative"
+              // imgSrc="/about/developer.png"
+              imgSrc="/hero/smridha3.png"
+            />
+          </div>
+          {/* tabs */}
+          <div className="flex-1">
+            <Tabs defaultValue="personal">
+              <TabsList className="w-full grid xl:grid-cols-4 xl:max-w-[520px] xl:border dark:border-none">
+                <TabsTrigger className="w-[126px] xl:w-auto" value={"personal"}>
+                  Personal Info
+                </TabsTrigger>
+                <TabsTrigger
+                  className="w-[126px] xl:w-auto"
+                  value={"qualifications"}
+                >
+                  Qualifications
+                </TabsTrigger>
+                <TabsTrigger className="w-[126px] xl:w-auto" value={"skills"}>
+                  Skill
+                </TabsTrigger>
+                <TabsTrigger
+                  className="w-[126px] xl:w-auto"
+                  value={"communities"}
+                >
+                  Communities
+                </TabsTrigger>
+              </TabsList>
+              {/* tabs content */}
+              <div className="text-lg mt-12 xl:mt-8">
+                <TabsContent value={"personal"}>personal</TabsContent>
+                <TabsContent value={"qualifications"}>
+                  qualifications
+                </TabsContent>
+                <TabsContent value={"skills"}>skills</TabsContent>
+                <TabsContent value={"communities"}>communities</TabsContent>
+              </div>
+            </Tabs>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- constants/AboutInfo.tsx
+++ constants/AboutInfo.tsx
@@ -6,7 +6,6 @@ import {
   PhoneCall,
   User2,
 } from "lucide-react";
-import { text } from "stream/consumers";
 
 export const ABOUT_INFO = [
   {
@@ -32,5 +31,74 @@ export const ABOUT_INFO = [
   {
     icon: <HomeIcon size={20} />,
     text: "Kolkata, India",
+  },
+];
+
+export const ABOUT_QUALIFICATION = [
+  {
+    title: "education",
+    data: [
+      {
+        university: "Future Insititute of Engineering and Mangement",
+        qualification: "Bachelor of Technology in CSE",
+        years: "2020 - 2024",
+      },
+      {
+        university: "Future Campus School",
+        qualification: "Class 12 (Computer Science)",
+        years: "2018 - 2020",
+      },
+      {
+        university: "B. D. M. International",
+        qualification: "Class 10",
+        years: "2018",
+      },
+    ],
+  },
+  {
+    title: "experience",
+    data: [
+      {
+        company: "Helix Research and Development",
+        role: "Software Developer",
+        years: "May 2024 - Present",
+      },
+      {
+        company: "Aeonix Research and Innovations",
+        role: "Frontend Developer Intern",
+        years: "Aug 2023 - Feb 2024",
+      },
+    ],
+  },
+];
+
+export const ABOUT_SKILLS = [
+  {
+    title: "skills",
+    data: [
+      {
+        name: "HTML, CSS, Javascript, Typescript, SQL, C/C++",
+      },
+      {
+        name: "React.js, Next.js, TailwindCss, Sass, ",
+      },
+      {
+        name: "Node.js, Express.js",
+      },
+      {
+        name: "MongoDB",
+      },
+    ],
+  },
+  {
+    title: "tools",
+    data: [
+      {
+        imgPath: "/about/vscode.svg",
+      },
+      {
+        imgPath: "/about/figma.svg",
+      },
+    ],
   },
 ];
