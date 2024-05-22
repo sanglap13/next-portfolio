a55ad930ebea834bd81290d5f5940a8ca2945157
sanglap13 <sanglapmridha@gmail.com>
2024-05-22 23:30:16 +0530

    refactor: add new pic in about section

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -20,7 +20,7 @@ const About = () => {
             <DevImg
               containerStyles="bg-about_shape_light dark:bg-about_shape_dark w-[505px] h-[505px] bg-no-repeat relative"
               // imgSrc="/about/developer.png"
-              imgSrc="/about/developer.png"
+              imgSrc="/about/sanglapmridha.png"
             />
           </div>
           {/* tabs */}
