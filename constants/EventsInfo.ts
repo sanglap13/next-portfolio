06f50772a4b05e98f1d06706e5aa46ba1098a7da
sanglap13 <sanglapmridha@gmail.com>
2024-05-28 21:16:20 +0530

    feat: add events

--- constants/EventsInfo.ts
+++ constants/EventsInfo.ts
@@ -2,38 +2,39 @@ import { title } from "process";
 
 export const EVENTS_INFO = [
   {
-    img: "/techTalks/podcast.png",
-    title: "Podcast",
-    date: "12th August 2021",
+    img: "/events/gccdkolextended.png",
+    title: "GCCD Kolkata X GDSC FIEM",
+    date: "18th May 2024",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      "Google Cloud Community Kolkata Extended Event organised by GDG Cloud Kolkata & GDSC FIEM.",
   },
   {
-    img: "/techTalks/podcast.png",
-    title: "Podcast",
-    date: "12th August 2021",
+    img: "/events/phnx.png",
+    title: "Phoenix 2024 - Annual Tech Fest",
+    date: "12th-14th April 2024",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      "Phoenix is athe annual tech fest of FIEM. It is a three day event with various technical, gaming and non-technical events. ",
   },
   {
-    img: "/techTalks/podcast.png",
-    title: "Podcast",
-    date: "12th August 2021",
+    img: "/events/gdsclead.png",
+    title: "Next GDSC Lead of FIEM",
+    date: "15th March 2024",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      "Organised by GDSC FIEM where I shared my experience as a GDSC Lead and tips and tricks to ace the selection interview.",
   },
   {
-    img: "/techTalks/podcast.png",
-    title: "Podcast",
-    date: "12th August 2021",
+    img: "/events/connectwithcoreteam.png",
+    title: "Connect with Core Team",
+    date: "30th January 2024",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      "Organised by GDSC FIEM. A networking event which was also the sequel of Google Cloud Study Jams 2023.",
   },
+
   {
-    img: "/techTalks/podcast.png",
-    title: "Podcast",
-    date: "12th August 2021",
+    img: "/events/opensource.png",
+    title: "Open Source & Beyond",
+    date: "13th October 2023",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      "Organised by GDSC FIEM. On the ocation of hacktoberfest a speaker session on Open Source and its importance.",
   },
 ];
