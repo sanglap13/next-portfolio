dcb282b290fbc589bcc9c3b097a0f94f07244e2c
sanglap13 <sanglapmridha@gmail.com>
2024-05-28 22:00:01 +0530

    feat: add community page

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -1,7 +1,67 @@
-import React from "react";
+"use client";
+
+import { IProjectProps } from "@/@types/Project.types";
+import CommunityCard from "@/components/shared/CommunityCard";
+import ProjectCard from "@/components/shared/ProjectCard";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { EVENTS_INFO } from "@/constants/EventsInfo";
+import { PROJECT_INFO } from "@/constants/ProjectInfo";
+import React, { useState } from "react";
+
+//categories
+const uniqueCategories = [
+  "all projects",
+  // ...Array.from(new Set(PROJECT_INFO.map((project) => project.category))),
+  ...new Set(EVENTS_INFO.map((project) => project.event_name)),
+];
+console.log(uniqueCategories);
 
 const Community = () => {
-  return <div>Community</div>;
+  const [categories, setCategories] = useState(uniqueCategories);
+  const [category, setCategory] = useState("all projects");
+
+  const filteredProjects = EVENTS_INFO.filter((project) => {
+    return category === "all projects"
+      ? project
+      : project.event_name === category;
+  });
+  console.log(filteredProjects);
+  return (
+    <section className="min-h-screen pt-12">
+      <div className="container mx-auto">
+        <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
+          My Community Experience
+        </h2>
+        {/* tabs */}
+        <Tabs defaultValue={category} className="mb-24 xl:mb-48">
+          {/* <TabsList className="w-full grid h-full md:grid-cols-4 lg:max-w-[640px] mb-12 mx-auto md:border dark:border-none">
+            {categories.map((category, index) => {
+              return (
+                <TabsTrigger
+                  onClick={() => setCategory(category)}
+                  value={category}
+                  key={index}
+                  className="capitalize w-[162px] md:w-auto"
+                >
+                  {category}
+                </TabsTrigger>
+              );
+            })}
+          </TabsList> */}
+          {/* tabs content */}
+          <div className="text-lg xl:mt-8 grid grid-cols-1 lg:grid-cols-3 gap-4">
+            {filteredProjects.map((project, index) => {
+              return (
+                <TabsContent value={category} key={index}>
+                  <CommunityCard event={project} />
+                </TabsContent>
+              );
+            })}
+          </div>
+        </Tabs>
+      </div>
+    </section>
+  );
 };
 
 export default Community;
--- constants/EventsInfo.ts
+++ constants/EventsInfo.ts
@@ -7,6 +7,8 @@ export const EVENTS_INFO = [
     date: "18th May 2024",
     description:
       "Google Cloud Community Kolkata Extended Event organised by GDG Cloud Kolkata & GDSC FIEM.",
+    link: "/community",
+    event_name: "react js",
   },
   {
     img: "/events/phnx.png",
@@ -14,13 +16,17 @@ export const EVENTS_INFO = [
     date: "12th-14th April 2024",
     description:
       "Phoenix is athe annual tech fest of FIEM. It is a three day event with various technical, gaming and non-technical events. ",
+    link: "/community",
+    event_name: "next js",
   },
   {
     img: "/events/gdsclead.png",
     title: "Next GDSC Lead of FIEM",
     date: "15th March 2024",
     description:
-      "Organised by GDSC FIEM where I shared my experience as a GDSC Lead and tips and tricks to ace the selection interview.",
+      "Organised b GDSC FIEM where I shared my experience as a GDSC Lead and tips and tricks to ace the selection interview.",
+    link: "/community",
+    event_name: "react js",
   },
   {
     img: "/events/connectwithcoreteam.png",
@@ -28,6 +34,8 @@ export const EVENTS_INFO = [
     date: "30th January 2024",
     description:
       "Organised by GDSC FIEM. A networking event which was also the sequel of Google Cloud Study Jams 2023.",
+    link: "/community",
+    event_name: "react js",
   },
 
   {
@@ -36,5 +44,7 @@ export const EVENTS_INFO = [
     date: "13th October 2023",
     description:
       "Organised by GDSC FIEM. On the ocation of hacktoberfest a speaker session on Open Source and its importance.",
+    link: "/community",
+    event_name: "fullstack",
   },
 ];
