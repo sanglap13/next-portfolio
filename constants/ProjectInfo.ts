7ddad17ae61cd1c34969646d71af998a45b7499d
sanglap13 <sanglapmridha@gmail.com>
2024-07-02 18:14:26 +0530

    refactor: minor changes

--- constants/ProjectInfo.ts
+++ constants/ProjectInfo.ts
@@ -5,7 +5,7 @@ export const PROJECT_INFO = [
     name: "DashDesk - Data Visualization Platform",
     description:
       "A cutting-edge Data Visualization Dashboard designed to empower businesses and individuals.",
-    link: "/",
+    link: "/projects",
     github: "https://github.com/sanglap13/data-visualization-dashboard",
   },
   {
@@ -14,7 +14,7 @@ export const PROJECT_INFO = [
     name: "SyncSlate - Collaborative Drawing Board",
     description:
       "An innovative web application designed to facilitate collaborative drawing experiences in real-time.",
-    link: "/",
+    link: "/projects",
     github: "https://github.com/sanglap13/sync-slate",
   },
   {
@@ -23,16 +23,16 @@ export const PROJECT_INFO = [
     name: "Nike Landing Page",
     description:
       "A responsive landing page designed for a fictitious Nike store.",
-    link: "/",
+    link: "/projects",
     github: "https://github.com/sanglap13/nike-landing-page",
   },
   {
     image: "/work/workout.png",
-    category: "fullstack",
+    category: "mern stack",
     name: "Workout Buddy",
     description:
       "A workout tracker application designed to help users stay fit and healthy.",
-    link: "/",
+    link: "/projects",
     github: "https://github.com/sanglap13/workout-tracker",
   },
 ];
